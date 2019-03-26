6855f24d791d5b376d11583c8ab841013b7b50c1
MYheavyGo <ga.sprunger@gmail.com>
2019-03-26 21:46:20 +0100

    Initial Commit

---
+++ POWERSHELL.ps1
@@ -0,0 +1,21 @@
+﻿$startTime = Get-Date
+Get-WmiObject -Class Win32_PowerPlan -Namespace "root\cimv2\power" | Select-Object ElementName, IsActive | Format-Table
+$elapsedTime = (Get-Date) - $startTime
+
+$totalTime = "{0:HH:mm:ss,fff}" -f ([datetime]$elapsedTime.Ticks)
+Write-Host $totalTime
+
+$startTime = Get-Date
+Get-CimInstance -Class Win32_PowerPlan -Name "root\cimv2\power" | Select-Object ElementName, IsActive | Format-Table
+$elapsedTime = (Get-Date) - $startTime
+
+$totalTime = "{0:HH:mm:ss,fff}" -f ([datetime]$elapsedTime.Ticks)
+Write-Host $totalTime
+
+powercfg /list
+powercfg /query 381b4222-f694-41f0-9685-ff5bb260df2e
+
+# PATH FOR DISPLAY SETTINGS
+$registryDPI = "HKCU:\Control Panel\Desktop"
+
+Test-Path $registryDPI
